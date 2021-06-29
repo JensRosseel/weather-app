3653d3498840f04cadc86c19e9da794431fe27ee
JensRosseel <slaythehorizon.games@gmail.com>
2021-06-29 21:40:44 +0200

    Add extra api functionality and basic css

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 (()=>{
     document.getElementById("submit").addEventListener("click", function(){
+        let date = new Date();
         const userCity = document.getElementById("userCity").value;
-        
+        let weather_charts = document.querySelector('.weather-charts');
+        weather_charts.innerHTML = '';
+
         async function getWeather(){
             let url = 'https://api.openweathermap.org/data/2.5/forecast?q='+userCity+'&units=metric&appid=ee5e6fca7948d3d060f7ae93a6118e21';
 
@@ -16,16 +19,16 @@
 
         async function displayWeather(){
             let weather = await getWeather();
+            console.log(weather);
             let html = '';
 
+            weather_charts.innerHTML += '<div class="city">Weather in '+weather.city.name+'</div>';
             for(let i = 0; i<5; i++){
-                let insert = '<div class="weather"><div class="day">'+'</div><div class="temp">Temp: '+weather.list[i].main.temp+'</div><div class="feels-like">Feels like: '+weather.list[i].main.feels_like+'</div><div class="humidity">Humidity: '+weather.list[i].main.humidity+'</div></div>';
+                let insert = '<div class="weather-card"><div class="day">'+'</div><div class="temp">'+weather.list[i].main.temp+' &#176;C </div><div class="feels-like">Feels like: '+weather.list[i].main.feels_like+' &#176;C</div><div class="weather">'+weather.list[i].weather[0].description+'</div><div class="humidity">Humidity: '+weather.list[i].main.humidity+'%</div><div class="wind">Wind speed: '+weather.list[i].wind.speed+' m/s</div></div>';
 
                 html += insert;
             }
-
-            let container = document.querySelector('.container');
-            container.innerHTML += html;
+            weather_charts.innerHTML += html;
         }
 
         displayWeather();
