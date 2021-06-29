a9a1f613aa167224cc285fe4e5b805c787749c94
JensRosseel <slaythehorizon.games@gmail.com>
2021-06-29 15:29:29 +0200

    Add favicon and api

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 (()=>{
-    
+    document.getElementById("submit").addEventListener("click", function(){
+        const userCity = document.getElementById("userCity").value;
+        fetch('https://api.openweathermap.org/data/2.5/forecast?q='+userCity+'&appid=ee5e6fca7948d3d060f7ae93a6118e21')
+            .then(response => response.json())
+            .then(data => console.log(data.list));
+    })
 })();
