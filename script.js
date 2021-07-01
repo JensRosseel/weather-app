0a9c90bbf3dc5e5cdd48f22111e8b697e7469d27
JensRosseel <slaythehorizon.games@gmail.com>
2021-07-01 16:50:32 +0200

    Fix days

--- script.js
+++ script.js
@@ -53,7 +53,11 @@
                 let insert = '<div class="weather-card"><div class="day">'+day+'</div><div class="temp">'+Math.round(weather.list[i].main.temp)+' &#176;C </div><div class="feels-like">Feels like: '+Math.round(weather.list[i].main.feels_like)+' &#176;C</div><div class="weather">'+weather.list[i].weather[0].description+'</div><div class="humidity">Humidity: '+weather.list[i].main.humidity+'%</div><div class="wind">Wind speed: '+weather.list[i].wind.speed+' m/s</div></div>';
 
                 html += insert;
-                counter++;
+                if(counter==7){
+                    counter = 1;
+                }else{
+                    counter++;
+                }
             }
             weather_charts.innerHTML += html;
         }
